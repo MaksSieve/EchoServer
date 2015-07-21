fd41c64a3b189641aa712809f2962d1d4415b672
Maxim <makssieve@gmail.com>
2015-07-21 14:09:27 +0300

    add files

---
+++ src/Client/Client.java
@@ -0,0 +1,61 @@
+package Client;
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+	
+	public static String welcomeMsg = "Welcome to Sieve-Client!";
+	public static String connectionClosedMsg = "Connection is closed!";
+	public static String byeMsg = "Client is closed. Good bye!";
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args)throws IOException  {
+		// TODO Auto-generated method stub
+		Socket Server = null;
+		int port;
+		Scanner sc;
+		BufferedReader serverIn = null;
+		BufferedReader userIn = null;
+		PrintWriter out = null;
+		
+		
+		System.out.println(welcomeMsg);
+		/*
+		if (args.length==0){
+			System.out.println("use: client hostname");
+			System.exit(-1);
+		}
+		*/
+		System.out.println("Input number of server's port:");
+		sc = new Scanner(System.in);
+		port = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Connecting to localhost"+/*args[0]+*/":"+port+" ...");
+		
+		Server = new Socket("localhost"/*args[0]*/,port);
+		serverIn = new BufferedReader(new InputStreamReader(Server.getInputStream()));
+		out = new PrintWriter(Server.getOutputStream(), true);
+		userIn = new BufferedReader(new InputStreamReader(System.in));
+		
+		String userMsg, serverMsg;
+		System.out.println("Connection sucessed!");
+		while ((userMsg = userIn.readLine())!=null){
+			out.println(userMsg);
+			serverMsg  = serverIn.readLine();
+			System.out.println(serverMsg);
+			if(userMsg.equalsIgnoreCase("close")) break;
+			if(userMsg.equalsIgnoreCase("exit")) break;
+		}
+		
+		out.close();
+		userIn.close();
+		serverIn.close();
+		Server.close();
+		System.out.println(connectionClosedMsg);
+		System.out.println(byeMsg);
+	}
+
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,84 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+	
+	public static String welcomeMsg = "Welcome to Sieve-Server!";
+	public static String byeMsg = "Sieve-Server is closed! Good bye!";
+	
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Initialization block
+		 */
+		
+		//ArrayList<User> cientList = new ArrayList<User>();
+		
+		BufferedReader in = null;
+		PrintWriter out = null;
+		Scanner sc = null;
+		
+		int port = 0;
+		ServerSocket Listener = null;
+		Socket Client = null;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println(welcomeMsg);
+		System.out.println("Server started");
+		
+		/*
+		 * Main block 
+		 */
+		
+		// Connecting to port
+		while (true){
+			try {
+				System.out.println("Input number of Server's port");
+				port = sc.nextInt();
+				Listener = new ServerSocket(port);
+				System.out.print(Listener.getInetAddress());
+				break;
+			}catch(IOException e) {
+				System.out.println("Couldn't listen to port " + port + "! Try again.");
+			}
+		}
+		sc.close();
+		
+		//Waiting client connections
+		System.out.println();
+		System.out.println("Waiting for clients' connections");
+		while (true){
+			try {
+				Client = Listener.accept();
+				System.out.println("Client "+ Client.getInetAddress()+" successfuly connected!");
+				break;
+			}catch(IOException e) {
+				System.out.println("Accept fault! Waiting for another client...");
+			}
+		}
+		
+		in = new BufferedReader(new InputStreamReader(Client.getInputStream()));
+		out = new PrintWriter(Client.getOutputStream(), true);
+		
+		String input;
+		
+		System.out.println("Wait for messages!");
+		//out.println("");
+		while((input = in.readLine()) != null){
+			if (input.equalsIgnoreCase("exit")) break;
+			out.println(Client.getInetAddress()+" ::: " + input);
+			System.out.println("User "+Client.getInetAddress()+ ": "+input);
+		}
+		
+		System.out.println(byeMsg);
+		out.close();
+		in.close();
+		Client.close();
+		Listener.close();
+	}
+}
